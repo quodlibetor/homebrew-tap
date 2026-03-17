3e7dcea4218793a05d331bf88f037ef92a13d23d
Brandon W Maister <quodlibetor@gmail.com>
2026-03-17 15:19:00 +0000

    add a git-stack formula for my shell scripts

---
+++ Formula/git-stack.rb
@@ -0,0 +1,16 @@
+class GitStack < Formula
+  desc "Helpers for working with stacked PR branches (rebase, push, log)"
+  homepage "https://github.com/quodlibetor/dotfiles"
+  head "https://github.com/quodlibetor/dotfiles.git", branch: "master"
+  license "MIT"
+
+  def install
+    %w[git-rebasestack git-pushstack git-lgstack].each do |script|
+      bin.install "dot_local/bin/executable_#{script}" => script
+    end
+  end
+
+  test do
+    system "#{bin}/git-rebasestack", "-h"
+  end
+end
